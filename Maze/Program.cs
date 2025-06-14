2d5b0bb238493ede0891b75158c71545363026f3
Andrei Kovtun <andreykpq@gmail.com>
2025-06-14 16:44:27 +0200

    adds possibilitz to add dimensions

--- Maze/Program.cs
+++ Maze/Program.cs
@@ -1,7 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MazeGenerator;
 
+var length = GetDimension("Length");
+var width = GetDimension("Width");
+
+
 var maze = new Maze();
-var generatedMaze = maze.Generate(10, 10);
+var generatedMaze = maze.Generate(length, width);
 
 Maze.PrintMaze(generatedMaze);
+
+int GetDimension(string message)
+{
+    do {
+        Console.Write(message + " : ");
+        var input = Console.ReadLine();
+        if (int.TryParse(input, out var dimension) && dimension > 0)
+        {
+            return dimension;
+        }
+        Console.WriteLine("Invalid input. Please enter a positive integer. Or press Ctrl+C to exit.");   
+    } while (true);
+}
